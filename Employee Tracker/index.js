550b15d5c1074985d4293a1043cfa3b7f0341035
jmartincampos <jmartincampos18@gmail.com>
2024-04-20 13:34:40 -0700

    changes to db

--- Employee Tracker/index.js
+++ Employee Tracker/index.js
@@ -1,52 +1,11 @@
-// index.js
-const inquirer = require('inquirer');
-const { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole } = require('./db/queries');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Main menu
-function mainMenu() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'option',
-            message: 'What would you like to do?',
-            choices: [
-                'View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role'
-            ]
-        }
-    ]).then((answers) => {
-        switch (answers.option) {
-            case 'View all departments':
-                viewAllDepartments();
-                break;
-            case 'View all roles':
-                viewAllRoles();
-                break;
-            case 'View all employees':
-                viewAllEmployees();
-                break;
-            case 'Add a department':
-                addDepartment();
-                break;
-            case 'Add a role':
-                addRole();
-                break;
-            case 'Add an employee':
-                addEmployee();
-                break;
-            case 'Update an employee role':
-                updateEmployeeRole();
-                break;
-            default:
-                console.log('Invalid option');
-        }
-    });
-}
+// Set up routes or other middleware here
 
-// Start application
-mainMenu();
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
