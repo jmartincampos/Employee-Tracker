298bedd5426019de6dbb7265273ca4265fd162ba
jmartincampos <jmartincampos18@gmail.com>
2024-04-21 14:38:45 -0700

    commit

--- Employee Tracker/db/queries.js
+++ Employee Tracker/db/queries.js
@@ -1,5 +1,6 @@
 // queries.js
 const mysql = require('mysql2');
+const connect = require('./db');
 
 // Create connection pool
 const pool = mysql.createPool({
@@ -17,4 +18,36 @@ function viewAllDepartments() {
     });
 }
 
-// Other query functions (viewAllRoles, viewAllEmployees, addDepartment, etc.)
+// View all roles
+function viewAllRoles() {
+    pool.query('SELECT * FROM role', (err, results) => {
+        if (err) throw err;
+        console.table(results);
+    });
+}
+
+// View all employees
+function viewAllEmployees() {
+    pool.query('SELECT * FROM employee', (err, results) => {
+        if (err) throw err;
+        console.table(results);
+    });
+}
+
+// Add department
+function addDepartment(departmentName) {
+    pool.query('INSERT INTO department (name) VALUES (?)', [departmentName], (err, results) => {
+        if (err) throw err;
+        console.log('Department added successfully');
+    });
+}
+
+// Other query functions (addRole, addEmployee, updateEmployeeRole, etc.)
+
+module.exports = {
+    viewAllDepartments,
+    viewAllRoles,
+    viewAllEmployees,
+    addDepartment
+    // Add other functions as needed
+};
