710f80d53a54f1bb1ff53a0f1610a46aa5d88e3e
jmartincampos <jmartincampos18@gmail.com>
2024-04-24 21:07:04 -0700

    server.js

--- Employee Tracker/server.js
+++ Employee Tracker/server.js
@@ -1,12 +1,46 @@
 // server.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
+const mysql = require('mysql2')
 
 // Set up routes or other middleware here
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'books_db'
+  },
+  console.log(`Connected to the books_db database.`)
+);
+
+// Query database
+let deletedRow = 2;
+
+db.query(`DELETE FROM favorite_books WHERE id = ?`, deletedRow, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+// Query database
+db.query('SELECT * FROM favorite_books', function (err, results) {
+  console.log(results);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-module.exports = app;
